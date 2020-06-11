51cbc16258f6572d95b583e4f2e2cc8e223a6452
Babak Ansari-Jaberi <babakansari@yahoo.com>
2020-06-11 17:05:49 -0400

    Refactoring to pass player name instead of isMiniMax parameter

--- minimax.js
+++ minimax.js
@@ -4,67 +4,67 @@
 // https://youtu.be/I64-UTORVfU
 // https://editor.p5js.org/codingtrain/sketches/0zyUhZdJD
 
+const players = {
+  X: 10,
+  O: -10,
+  tie: 0
+};
+
 function bestMove() {
-    // AI to make its turn
+  // AI to make its turn
+  let bestScore = -Infinity;
+  let move;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      // Is the spot available?
+      if (board[i][j] == '') {
+        board[i][j] = ai;
+        let score = minimax(board, 0, players.X);
+        board[i][j] = '';
+        if (score > bestScore) {
+          bestScore = score;
+          move = { i, j };
+        }
+      }
+    }
+  }
+  board[move.i][move.j] = ai;
+  currentPlayer = human;
+}
+  
+function minimax(board, depth, player) {
+  let result = checkWinner();
+  if (result !== null) {
+    return players[result];
+  }
+
+  if (player === players.O) {
     let bestScore = -Infinity;
-    let move;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         // Is the spot available?
         if (board[i][j] == '') {
           board[i][j] = ai;
-          let score = minimax(board, 0, false);
+          let score = minimax(board, depth + 1, players.X);
           board[i][j] = '';
-          if (score > bestScore) {
-            bestScore = score;
-            move = { i, j };
-          }
+          bestScore = max(score, bestScore);
         }
       }
     }
-    board[move.i][move.j] = ai;
-    currentPlayer = human;
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        if (board[i][j] == '') {
+          board[i][j] = human;
+          let score = minimax(board, depth + 1, players.O);
+          board[i][j] = '';
+          bestScore = min(score, bestScore);
+        }
+      }
+    }
+    return bestScore;
   }
-  
-  let scores = {
-    X: 10,
-    O: -10,
-    tie: 0
-  };
-  
-  function minimax(board, depth, isMaximizing) {
-    let result = checkWinner();
-    if (result !== null) {
-      return scores[result];
-    }
-  
-    if (isMaximizing) {
-      let bestScore = -Infinity;
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          // Is the spot available?
-          if (board[i][j] == '') {
-            board[i][j] = ai;
-            let score = minimax(board, depth + 1, false);
-            board[i][j] = '';
-            bestScore = max(score, bestScore);
-          }
-        }
-      }
-      return bestScore;
-    } else {
-      let bestScore = Infinity;
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          // Is the spot available?
-          if (board[i][j] == '') {
-            board[i][j] = human;
-            let score = minimax(board, depth + 1, true);
-            board[i][j] = '';
-            bestScore = min(score, bestScore);
-          }
-        }
-      }
-      return bestScore;
-    }
-  } 
+} 
