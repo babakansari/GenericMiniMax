94157df9fcd11e8cf6b6cddecb0c70b3a1ab880a
Babak Ansari-Jaberi <babakansari@yahoo.com>
2020-06-12 12:19:13 -0400

    Make generic loop

--- transposer.js
+++ transposer.js
@@ -4,24 +4,16 @@ class transposer{
 
     }
 
-    onGetBestScore = function(board, i, j, bestScore, onGetScore){
-        if (board[i][j] == '') {
-            board[i][j] = ai;
-            let score = onGetScore();
-            board[i][j] = '';
-            bestScore = max(score, bestScore);
-        }
-        return bestScore;s
-    }
-
-    onIterate = function(board, onGetScore){
-        let bestScore = -Infinity;
+    onIterate = function(board, player, onGetScore){
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                bestScore = this.onGetBestScore(board, i, j, bestScore, onGetScore);
+                if (board[i][j] == '') {
+                    board[i][j] = (player == players.X) ? ai : human;
+                    onGetScore();
+                    board[i][j] = '';
+                }
             }
         }
-        return bestScore
     } 
   
     minimax = function(board, player) {
@@ -31,22 +23,18 @@ class transposer{
         }
 
         if (player === players.O) {
-            return this.onIterate(board, ()=>{
-                return this.minimax(board, players.X);
+            let bestScore = -Infinity;
+            this.onIterate(board, players.X, ()=>{
+                let score = this.minimax(board, players.X);
+                bestScore = max(score, bestScore);
             });
+            return bestScore;
         } else {
             let bestScore = Infinity;
-            for (let i = 0; i < 3; i++) {
-                for (let j = 0; j < 3; j++) {
-                    // Is the spot available?
-                    if (board[i][j] == '') {
-                        board[i][j] = human;
-                        let score = this.minimax(board, players.O);
-                        board[i][j] = '';
-                        bestScore = min(score, bestScore);
-                    }
-                }
-            }
+            this.onIterate(board, players.O, ()=>{
+                let score = this.minimax(board, players.O);
+                bestScore = min(score, bestScore);
+            });
             return bestScore;
         }
     } 
